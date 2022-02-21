0e76e296eae9f08be19815236ef89b5f8580c2c0
JWViking <julianawilcox@gmail.com>
2022-02-21 11:20:38 -0700

    setinterval attempt

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,17 @@
 setInterval(quizTimer, 1000);
 
-function quizTimer () {
-    if(i=0, i>0, i++); 
-};
+var i= 60;
+
+var start = addEventListener("click", ".start-btn", startClickHandler);
+
+// if click on start, start timer.
+
+function startClickHandler () {
+    //If  i is 1-60 i--. display score every second.
+    if(i=60, i>0, i--);
+        setInterval(function() {
+            getElementById("score-id").innerHTML = i;
+            console.log(i);
+        }
+
+    }, 1000);
