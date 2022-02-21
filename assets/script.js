a32cdcb67c0a824752c120551f3af3727e830de3
JWViking <julianawilcox@gmail.com>
2022-02-21 16:09:12 -0700

    event listener try again

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,57 @@
-setInterval(quizTimer, 1000);
+var startButtonEl = document.getElementById('start');
+var scoreEl = document.getElementById('score-id');
 
-var i= 60;
+startButtonEl.addEventListener("click", startHandler);
 
-var start = addEventListener("click", ".start-btn", startClickHandler);
+function startHandler() {
+    var startTime = 60;
 
-// if click on start, start timer.
-
-function startClickHandler () {
-    //If  i is 1-60 i--. display score every second.
-    if(i=60, i>0, i--);
-        setInterval(function() {
-            getElementById("score-id").innerHTML = i;
-            console.log(i);
+    var timer = setInterval(function() {
+        if (startTime >10 && startTime<61) {
+            scoreEl.textContent = startTime;
+            startTime --;
+            console.log(startTime);
         }
+        else if (startTime >0 && startTime <11) {
+            scoreEl.textContent = startTime + 'Seconds Left';
+            let scoreEl = ".hurry-up";
+            
+        }
+        else {
+            startTime = "Time Is Out"
+        }
+    })  
+};
 
-    }, 1000);
+
+
+
+
+
+// //setInterval(quizTimer, 1000);
+
+// var i= 60;
+// var startButton = getElementById("start");
+// startButton.addEventListener("click", startClickHandler);
+
+// // if click on start, start timer.
+
+// function startClickHandler () {
+//     //If  i is 1-60 i--. display score every second.
+//     // if(i=60, i>0, i--) {
+//     //     setInterval(function() {
+//     //         getElementById("score-id").innerHTML = i;
+//     //         console.log(i);
+//     //     }, 1000);
+
+//     }
+// }
+
+//Question 1:
+
+
+//Question 2:
+
+//Question 3:
+
+//Queestion 4:
